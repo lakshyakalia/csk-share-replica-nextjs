69bd04b0c3f4f1743c55cf2f1d725dd5f4b8eb88
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2025-02-11 08:00:51 +0530

    Changed type any to more specific type

--- src/app/api/getInitialData/route.ts
+++ src/app/api/getInitialData/route.ts
@@ -6,23 +6,23 @@ import fs from "fs";
 export const GET = () => {
   try {
     const filePath = path.resolve("./public/assets/csk_initial_data.xlsx");
-
     const fileBuffer = fs.readFileSync(filePath);
-
     const workbook = XLSX.read(fileBuffer, { type: "buffer" });
 
-    const allSheetsData: { [key: string]: any[] } = {};
+    const allSheetsData: { [key: string]: Record<string, unknown>[] } = {};
 
     workbook.SheetNames.forEach((sheetName) => {
       const worksheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      const jsonData =
+        XLSX.utils.sheet_to_json<Record<string, unknown>>(worksheet);
       allSheetsData[sheetName] = jsonData;
     });
 
     return new NextResponse(JSON.stringify(allSheetsData), { status: 200 });
   } catch (err) {
-    return new NextResponse(JSON.stringify("Error reading Excel file:" + err), {
-      status: 500,
-    });
+    return new NextResponse(
+      JSON.stringify("Error reading Excel file: " + (err as Error).message),
+      { status: 500 }
+    );
   }
 };
